a7f9522da91e8230d5c8788f414b2a8126e8fc41
inczDan <daniel.alvmatos@gmail.com>
2023-11-25 21:53:56 -0300

    dockeriza banco de dados e backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
