e11fa87e36966a40f4dc85440ea9e8cfa18a1b56
inczDan <daniel.alvmatos@gmail.com>
2023-11-30 16:41:55 -0300

    adiciona endpoint para visualizar perfil de vendedor

--- src/controllers/vendedor/obterPerfilVendedor.js
+++ src/controllers/vendedor/obterPerfilVendedor.js
@@ -1,3 +1,7 @@
-const obterPerfilVendedor = async (req, res) => {};
+const obterPerfilVendedor = async (req, res) => {
+  const { senha: _, cpf: __, ...perfilvendedor } = req.vendedor;
+
+  return res.status(200).json(perfilvendedor);
+};
 
 module.exports = { obterPerfilVendedor };
