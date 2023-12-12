dc868c97624f44e004577a5323524b2732622f80
inczDan <daniel.alvmatos@gmail.com>
2023-12-12 14:19:33 -0300

    implementa teste na homepage

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 
 const rotasVendedor = require('./routes/vendedor-rota');
 const rotasCategoria = require('./routes/categoria-rota');
-const rotasTeste = require('./routes/teste-rota');
+const homepage = require('./routes/hpage');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(rotasTeste);
+app.use(homepage);
 app.use(rotasCategoria);
 app.use(rotasVendedor);
 
